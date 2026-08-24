17b3f4b9b7e7b7c07a553c733cbadc0bcaec5bc9
DERRICK-LEGACY <legacyderrick12@gmail.com>
2026-08-24 20:40:49 +0300

    Completely remove Neon adapter to fix connection termination on Vercel Node.js Serverless

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from '@prisma/config'
 export default defineConfig({
   earlyAccess: true,
   schema: "prisma/schema.prisma",
+  // Vercel injected environment variable
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
 })
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,7 @@
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
-
-const connectionString = `${process.env.DATABASE_URL_UNPOOLED || process.env.POSTGRES_URL_NON_POOLING || process.env.DATABASE_URL}`.replace('?pgbouncer=true', '').replace('&pgbouncer=true', '')
-
-const pool = new Pool({ connectionString })
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const adapter = new PrismaNeon(pool as any)
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare global {
