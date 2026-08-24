6195198eb007765fd1ddb54b766d58473b569762
DERRICK-LEGACY <legacyderrick12@gmail.com>
2026-08-24 23:46:00 +0300

    Revert to standard Prisma client without adapters to fix Vercel crashes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { neon } from '@neondatabase/serverless'
-import { PrismaNeonHTTP } from '@prisma/adapter-neon'
-
-const connectionString = `${process.env.DATABASE_URL}`
-
-const sql = neon(connectionString)
-const adapter = new PrismaNeonHTTP(sql)
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare global {
