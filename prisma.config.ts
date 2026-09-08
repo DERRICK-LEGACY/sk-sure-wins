24cf67a4d016e886fc98c67156874bd456dadbc3
DERRICK-LEGACY <legacyderrick12@gmail.com>
2026-09-08 11:06:03 +0300

    Fix database schema missing on production

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from '@prisma/config'
+import { config } from 'dotenv'
+config()
 
 export default defineConfig({
   earlyAccess: true,
   schema: "prisma/schema.prisma",
-  // Vercel injected environment variable
   datasource: {
     url: process.env.DATABASE_URL,
   },
